8a7dfe1f1123476d28c3c59b9df16cef367ea274
ansh-n-chovatiya <ansh.n.chovatiya@gmail.com>
2026-07-13 22:39:32 +0530

    13 July

---
+++ python-basics/data-types.py
@@ -0,0 +1,49 @@
+print("Sum", 2 + 4)
+print("Minus", 5 - 2)
+print("Multiply", 4 * 8)
+print("Divide", 8 / 4)
+
+
+print(type(4))
+print(type(-4))
+print(type(4.9))
+
+print(4 + 1.1, type(4 + 1.1))
+print(4.9 + 5.1, type(4.9 + 5.1))
+
+print(5**3)
+print(2 // 4)
+print(225 // 4)
+print(225 / 4)
+print(219 // 4)
+print(219 / 4)
+
+
+testVar = 3
+testVar2 = 4
+
+x, y, z = 4, 3, 8
+
+print(testVar + testVar2)
+print(x, y, z)
+
+
+print(type("Heloo there"))
+
+
+lString = """
+Hello,
+
+this is multiline big string
+
+bottom
+"""
+
+print(lString)
+
+first_name = "ansh"
+last_name = "chovatiya"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
---
+++ python-basics/math-functions.py
@@ -0,0 +1,14 @@
+import math
+
+print(abs(43.44))
+print(abs(-43.89))
+print(round(43.44))
+print(round(43.89))
+
+print(math.ceil(43.44))
+print(math.floor(43.44))
+
+print(bin(1))
+print(bin(5))
+
+print(int("0b101", 2))
